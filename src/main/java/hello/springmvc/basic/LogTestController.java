f2c4020dcec545386442c1a1a2e2231be2f4fa3c
sudong97 <tkddlsqkr21@gmail.com>
2023-07-02 20:00:37 +0900

    first commit log

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,22 @@
+package hello.springmvc.basic;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+        log.trace("trace log={}11", name);
+        log.debug("debug log={}22", name);
+        log.info(" info log = {}33", name);
+        log.warn("warn log={}44", name);
+        log.error("error log={}55", name);
+        return "ok";
+    }
+}
